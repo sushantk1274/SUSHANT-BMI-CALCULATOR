2580a12c581045c6ded98a458147f55ed63dfa29
SUSHANT KUMAR <97659153+sushantk1274@users.noreply.github.com>
2023-01-16 01:39:00 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,32 @@
+package caps.sushant.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextWeight = findViewById(R.id.weight);
+        EditText editTextHeight = findViewById(R.id.height);
+        Button button = findViewById(R.id.submit);
+        TextView textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText())) / 100;
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi = weight/(height * height);
+                textView.setText(String.valueOf(bmi));
+            }
+        });
+    }
+}
